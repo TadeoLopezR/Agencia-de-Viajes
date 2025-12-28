31cba32df35f29b382519453504acc84bd0230d1
Tadeo Lopez <lrstadeo@hotmail.com>
2025-12-28 19:17:14 -0300

    server funcionando y mostrando datos

--- src/pages/Viajes.jsx
+++ src/pages/Viajes.jsx
@@ -1,9 +1,27 @@
+import { useState, useEffect } from 'react';
 
 function Viajes() {
-
+    const [data, setData] = useState([]);
+    useEffect(() => {
+    fetch('http://localhost:3000/api/viajes')
+      .then(res => res.json())
+      .then(data => setData(data));
+  }, []);
+  
     return(
-        
-        <h1>Hola Viajes</h1>
+        <main className="container mt-5">
+            <h1>HOLa ViAJES</h1>
+            <div>
+                {data.map(({ id, ...rest }) => (
+            <div key={id}>
+            {Object.entries(rest).map(([key, value]) => (
+            <p><strong>{key}:</strong> {value}</p>
+          ))}
+        </div>
+        ))}
+    </div>
+            
+        </main>
     )
 }
 
